21693fb2c1ebabc0d68e564b07d5db32c1677921
Evelyne <e.umubyeyi@alustudent.com>
2023-03-14 09:23:38 +0200

    feat(screens/long_break_timer): added screen for long break timer in between sessions
    feat(screens/short_break_timer): added screen for short break timer in between sessions
    
    feat(screens/sesseion_timer): added screen for session timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:achievers_app/firebase_options.dart';
 import 'package:achievers_app/screens/home.dart';
 import 'package:achievers_app/screens/onboarding_screen.dart';
 import 'package:achievers_app/screens/sign_in_screen.dart';
 import 'package:achievers_app/screens/sign_up_screen.dart';
 import 'package:achievers_app/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+import 'package:achievers_app/screens/session_timer.dart';
+import 'package:achievers_app/screens/short_break_timer.dart';
+import 'package:achievers_app/screens/long_break_timer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -16,13 +24,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.put(LongBreakTimerController());
     return MaterialApp(
       title: 'Task Achievers App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.deepPurple,
           textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
-      home: SplashScreen(),
+      home: LongBreakTimerScreen(),
     );
   }
 }
---
+++ lib/screens/short_break_timer.dart
@@ -0,0 +1,292 @@
+import 'dart:async';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class BreakTimerController extends GetxController {
+  Timer? _timer;
+  int remainingSeconds = 300;
+  final time = '5:00'.obs;
+  bool running = false;
+  final percentage = '1'.obs;
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    super.onClose();
+  }
+
+  _startTimer() {
+    running = true;
+    const duration = Duration(seconds: 1);
+    _timer = Timer.periodic(duration, (Timer timer) {
+      if (remainingSeconds == 0) {
+        timer.cancel();
+      } else {
+        int minutes = remainingSeconds ~/ 60;
+        int seconds = (remainingSeconds % 60);
+        time.value = minutes.toString().padLeft(2, "0") +
+            ":" +
+            seconds.toString().padLeft(2, "0");
+        percentage.value = (remainingSeconds / 300).toString();
+        remainingSeconds--;
+      }
+    });
+  }
+}
+
+class ShortBreakTimerScreen extends StatefulWidget {
+  @override
+  State<ShortBreakTimerScreen> createState() => _ShortBreakTimerScreen();
+}
+
+class _ShortBreakTimerScreen extends State<ShortBreakTimerScreen> {
+  final controller = Get.put(BreakTimerController());
+
+  IconData icon_value = Icons.play_arrow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: false,
+            title: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                ),
+                // child: Icon(Icons.arrow_back, color: Colors.black,),
+                Text(
+                  'Sessions Timer',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: Container(
+            margin: EdgeInsets.fromLTRB(18, 10, 15, 0),
+            width: MediaQuery.of(context).size.width,
+            child: Column(children: [
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFF04060F).withOpacity(0.05),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Card(
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    elevation: 0,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ListTile(
+                        autofocus: false,
+                        leading: Container(
+                          height: 45,
+                          width: 45,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            // 'Learn Programming', 0xFF00A9F1, Icons.code, '120 minutes'
+                            color: Color(0xFF00A9F1),
+                          ),
+                          child: Center(
+                            child: Icon(
+                              Icons.code,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ),
+                        ),
+                        title: Text(
+                          "Learn Programming",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 16,
+                              color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          "120 minutes",
+                          style:
+                              TextStyle(fontSize: 11, color: Color(0xFF7C7575)),
+                        ),
+                        trailing: Container(
+                          margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                          //     decoration: BoxDecoration(
+                          //       border: Border.all(width: 1, color: Colors.red),
+                          //     ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "1/4",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w900,
+                                    fontSize: 16,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                "25 minutes",
+                                style: TextStyle(
+                                    fontSize: 11, color: Color(0xFF7C7575)),
+                              ),
+                            ],
+                          ),
+                        ))),
+              ),
+              Container(
+                child: Column(children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 50, 0, 40),
+                    child: Obx(() => CircularPercentIndicator(
+                          radius: 140.0,
+                          percent: double.parse(controller.percentage.value),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          lineWidth: 25.0,
+                          progressColor: Color(0xFFffd300),
+                          backgroundColor: Color(0xFFfff8d9),
+                          center: Container(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Obx(() => Text('${controller.time.value}',
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w900,
+                                  ))),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 17, 0, 0),
+                                child: Text(
+                                  "Short break",
+                                  style: TextStyle(
+                                      fontSize: 14, color: Color(0xFF7C7575)),
+                                ),
+                              ),
+                            ],
+                          )),
+                        )),
+                  ),
+                  Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                      child: Center(
+                        child: Text(
+                          "Take a short break for 5 minutes",
+                          style:
+                              TextStyle(fontSize: 14, color: Color(0xFF7C7575)),
+                        ),
+                      )),
+                  Container(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: Color(0xFFf0f0f0)),
+                              child: Center(
+                                  child: IconButton(
+                                      icon: const Icon(Icons.refresh),
+                                      color: Color(0xFFcccccc),
+                                      onPressed: () {
+                                        controller._timer!.cancel();
+                                        controller.remainingSeconds = 300;
+                                        // controller._startTimer();
+                                        controller.time.value = '5:00';
+                                        controller.running = false;
+                                        controller.percentage.value = '1';
+                                        setState(() {
+                                          icon_value = Icons.play_arrow;
+                                        });
+                                      })),
+                            ),
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xFFFFD300),
+                                      Color.fromRGBO(255, 211, 0, 0.75),
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                  )),
+                              child: Center(
+                                  child: IconButton(
+                                      icon: new Icon(icon_value),
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        if (controller.running) {
+                                          controller._timer!.cancel();
+                                          controller.running = false;
+                                          setState(() {
+                                            icon_value = Icons.play_arrow;
+                                          });
+                                        } else {
+                                          controller._startTimer();
+                                          setState(() {
+                                            icon_value = Icons.pause;
+                                          });
+                                        }
+                                      })),
+                            ),
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: Color(0xFFf0f0f0)),
+                              child: Center(
+                                  child: IconButton(
+                                      icon: new Icon(Icons.stop),
+                                      color: Color(0xFFcccccc),
+                                      onPressed: () {
+                                        if (controller.running) {
+                                          controller._timer!.cancel();
+                                          controller.running = false;
+                                          setState(() {
+                                            icon_value = Icons.play_arrow;
+                                          });
+                                        }
+                                      })),
+                            )
+                          ]),
+                    ),
+                  )
+                ]),
+              )
+            ])));
+  }
+}
